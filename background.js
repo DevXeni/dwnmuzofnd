6ae9d97f98c88a240f9f2d759f11a84bc139f4cc
Xeny <xxxxxx586@gmail.com>
2026-01-06 00:45:26 +0300

    ready for test

--- background.js
+++ background.js
@@ -1,36 +1,31 @@
+/*
+* File: background.js
+* Description: background script
+*
+* @author Kseniya Nikula aka DevXeni
+* @version 1.0.0
+* @copyright Copyright (c) 2026, Kseniya Nikula. All rights reserved.
+*
+*/
+function onStartedDownload(id) {
+    console.log("Started downloading: ${id}");
+}
 
-if(window.location.hostname == "muzofond.fm") {
-const allDownloadableLinks = document.querySelectorAll("ul.mainSongs li.item");
-for(let i=0; i < allDownloadableLinks.length-1; i++){
-    let linkContainer = allDownloadableLinks[i].querySelector("li.play");
-    let fileName = allDownloadableLinks[i].querySelector("span.track").innerText;
-    linkContainer.addEventListener("click", function(){
-        let fileToDownload = event.currentTarget.getAttribute("data-url");
-        console.log(fileToDownload)
-        console.log(fileName);
-        /*let linkToDownload = document.createElement("a");
-        linkToDownload.href = fileToDownload;
-        linkToDownload.download = fileName;
-        linkToDownload.style.display = 'none';
-        document.body.appendChild(linkToDownload);
-        linkToDownload.click(function(){prompt(fileName)});
-        document.body.removeChild(linkToDownload);*/
-        function onStartedDownload(id) {
-            console.log(`Started downloading: ${id}`);
-        }
+function onFailed(error) {
+    console.log("Download failed: ${error}");
+}
 
-        function onFailed(error) {
-            console.log(`Download failed: ${error}`);
-        }
-
-
-        let downloading = chrome.downloads.download({//browser.downloads.download({
-           url: fileToDownload,
-           //filename: fileName,
-           //conflictAction: "uniquify",
-           //saveAs: true
-        });
-
-        downloading.then(onStartedDownload, onFailed);
-    }, false);
-}}
+function downloadFile(message) {
+    if(message === undefined || message === null) {
+        console.log("Message not found");
+        return false
+    }
+    let downloading = browser.downloads.download({
+        url: message.url,
+        filename: fileName,
+        conflictAction: "uniquify",
+        saveAs: true
+    });
+    downloading.then(onStartedDownload, onFailed);
+}       
+browser.runtime.onMessage.addListener(downloadFile);
--- content.js
+++ content.js
@@ -1,16 +1,23 @@
-
 /*
-* author: Kseniya Nikula
+* File: background.js
+* Description: content script
+*
+* @author Kseniya Nikula aka DevXeni
+* @version 1.0.0
+* @copyright Copyright (c) 2026, Kseniya Nikula. All rights reserved.
+*
 */
-if(window.location.hostname == "muzofond.fm") {
+
 const allDownloadableLinks = document.querySelectorAll("ul.mainSongs li.item");
 for(let i=0; i < allDownloadableLinks.length-1; i++){
     let linkContainer = allDownloadableLinks[i].querySelector("li.play");
     let fileName = allDownloadableLinks[i].querySelector("span.track").innerText;
     linkContainer.addEventListener("click", function(){
         let fileToDownload = event.currentTarget.getAttribute("data-url");
-        console.log(fileToDownload)
-        console.log(fileName);
-        browser.runtime.sendMessage({url:fileToDownload,name:fileName })
+        if (fileToDownload) {
+            console.log(fileToDownload)
+            console.log(fileName);
+            browser.runtime.sendMessage({url:fileToDownload,name:fileName })
+        }
     }, false);
-}}
+}
